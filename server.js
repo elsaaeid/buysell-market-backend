6be5e2987bf3b15ebf4799ac116983701c68b3ea
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2026-01-18 01:51:16 +0200

    deploy chnages

--- server.js
+++ server.js
@@ -59,10 +59,6 @@ cloudinary.config({
 
 // Routes
 app.get("*", (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
   res.send("Home Page");
 });
 app.get("/favicon.ico", (req, res) => res.status(204));
